2b1648b4014bb84b7983525bad53a91656141697
name <email>
2023-12-09 22:35:59 +0500

    Day 9

---
+++ 9/1.py
@@ -0,0 +1,18 @@
+# s = 0
+def func(nums):
+    next_nums = []
+    for i in range(1,len(nums)):
+        next_nums.append(nums[i]-nums[i-1])
+    if len(set(next_nums))==1:
+        next_ = nums[-1] + next_nums[0]
+        return next_
+    return nums[-1] + func(next_nums)
+
+with open('input.txt','r') as f:
+    lines = f.read().strip().split('\n')
+
+res = 0
+for line in lines:
+    nums = map(int,(x for x in line.split() if x))
+    res += func(tuple(nums))
+print(res)
